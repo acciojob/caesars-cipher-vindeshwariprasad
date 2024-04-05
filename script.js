f4fb8c3dcb79933b0117d90b03e9f6ed07a6a71d
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-04-05 08:54:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function rot13(encodedStr) {
 		decodedArr.push(lookup[e]);
 	}
 
-  return decodedArr.join(""); //return decodedArr
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
